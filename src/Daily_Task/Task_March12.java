1ab445373182a25438c6965d82e3490033f5b956
Rupam Singh <reply4rupam@gmail.com>
2025-03-20 23:13:38 +0530

    Task 12th March 2025

---
+++ src/Daily_Task/Task_March12.java
@@ -0,0 +1,18 @@
+package Daily_Task;
+
+public class Task_March12 {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 10;
+        int z = 10;
+
+        double result = Math.cbrt(Math.pow(x, 2) + Math.pow(y, 2) - Math.abs(z));
+        System.out.println(result);
+
+
+
+
+
+    }
+}
